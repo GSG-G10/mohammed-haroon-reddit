895e637dbdae3ba2a12caa0e836b841e4300be0f
muhammadharoun <hro19502001@gmail.com>
2021-08-29 15:15:31 +0300

    add post handler

--- server/controllers/signupHandler.js
+++ server/controllers/signupHandler.js
@@ -1,8 +1,7 @@
-const { join } = require('path');
 const { addUserQuery, } = require('../database/Queries')
 
 
 module.exports = (req, res) => {
-    addUserQuery('11dcscvsv','dsvsv@gmail.com','usscername','dcspassword')
-    res.send('good')
-  };
+    addUserQuery(req.body)
+    res.redirect('/')
+};
