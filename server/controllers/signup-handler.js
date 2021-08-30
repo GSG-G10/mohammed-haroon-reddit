e38bce66a7cf0f702e5d8a598526c04a081f2c5e
muhammadharoun <hro19502001@gmail.com>
2021-08-30 10:32:01 +0300

    create and send cookies function

--- server/controllers/signup-handler.js
+++ server/controllers/signup-handler.js
@@ -1,10 +1,11 @@
 const { addUserQuery, } = require('../database/Queries')
 const {hashPassword, } = require('../middlewares/hash-password')
+const loginToken= require('../middlewares/login-cookies')
 
 module.exports = (req, res) => {
     // server side validation
     hashPassword(req.body.password)
     .then(hashpass => addUserQuery(req.body.name,req.body.email,req.body.username,hashpass))
     .catch(err => console.log(err))
-    res.redirect('/')
+    loginToken(req, res)
 };
