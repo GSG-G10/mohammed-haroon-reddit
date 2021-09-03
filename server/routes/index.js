fae86dbcf47b47242ac55df3ec32ab0ce426d37f
muhammadharoun <hro19502001@gmail.com>
2021-09-04 00:18:32 +0300

    front side validation

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,20 +1,30 @@
 const router = require('express').Router();
+const { join } = require('path');
 
-const { homePage,signup,deletePost,getPosts,login,checkuser ,createpost,profiledata,profilepage} = require('../controllers');
-const checkIsUser = require('../middlewares/check-if-user')
-router.get('/',homePage);
-router.post('/signup',signup);
-router.post('/login',login);
-router.get('/checkuser',checkuser);
 
-router.get('/deletePost',checkIsUser,deletePost);
-router.get('/getPosts',checkIsUser,getPosts);
-router.get('/createpost',checkIsUser,createpost);
-router.get('/profiledata/:username',profiledata);
-router.get('/profilepage/:username',profilepage);
+const {homePage,signup,deletePost,getPosts,login,checkuser,createpost,profiledata,profilepage,} = require('../controllers');
+const checkIsUser = require('../middlewares/check-if-user');
+router.get('/', homePage);
+router.post('/signup', signup);
+router.post('/login', login);
+router.get('/checkuser', checkuser);
+
+router.get('/deletePost', checkIsUser, deletePost);
+router.get('/getPosts', checkIsUser, getPosts);
+router.get('/createpost', checkIsUser, createpost);
+router.get('/profiledata/:username', profiledata);
+router.get('/profilepage/:username', profilepage);
 router.post('/logout', (req, res) => {
   res.clearCookie('logged').redirect('/');
 });
-profilepage
+router.get('/signup', (req, res) => {
+  res.sendFile(join(__dirname, '..', '..', 'public', 'signup.html'));
+});
+router.get('/login', (req, res) => {
+  res.sendFile(join(__dirname, '..', '..', 'public', 'login.html'));
+});
+router.get('/signup', (req, res) => {
+  res.sendFile(join(__dirname, '..', '..', 'public', 'profile.html'));
+});
 
 module.exports = router;
